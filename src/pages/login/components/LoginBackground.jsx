8d5884ca9da4facbe995236b6c16774f450db964
Adrian Rudnicki <adi2602@wp.pl>
2023-03-09 11:52:56 +0100

    Fix button and add modal for login form

--- src/pages/login/components/LoginBackground.jsx
+++ src/pages/login/components/LoginBackground.jsx
@@ -1,23 +1,14 @@
 import { Box } from "@chakra-ui/react";
-import beer_background from "../../../assets/graphics/beer_background.jpg";
 
 export default function LoginBackground({ children }) {
   return (
     <Box
-      backgroundImage={beer_background}
-      backgroundPosition="center"
-      backgroundRepeat="no-repeat"
-      backgroundSize="cover"
       minHeight="100vh"
+      bgColor="blackAlpha.900"
+      backdropFilter="auto"
+      backdropBrightness="40%"
     >
-      <Box
-        minHeight="100vh"
-        backdropFilter="auto"
-        backdropBlur="3px"
-        backdropBrightness="50%"
-      >
-        {children}
-      </Box>
+      {children}
     </Box>
   );
 }
---
+++ src/pages/login/components/LoginModal.jsx
@@ -0,0 +1,24 @@
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
+
+export default function LoginModal({ form, isOpen, onClose }) {
+  return (
+    <Modal isCentered isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay bg="none" backdropFilter="auto" backdropBlur="6px" />
+      <ModalContent bgColor="#1a1a1a">
+        <ModalHeader>
+          <Text color="whiteAlpha.800">Zaloguj się</Text>
+        </ModalHeader>
+        <ModalCloseButton color="whiteAlpha.800" />
+        <ModalBody>{form}</ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
